8451bacc3c152cd57122e7bfc47393a3d30c30d3
Ryan <ductoman16@gmail.com>
2025-04-21 23:09:07 -0400

    Add test project and fix CI pipeline

---
+++ DoNotMock.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+
+namespace DoNotMock.Tests;
+
+public class DoNotMockAttributeTests
+{
+    [Fact]
+    public void Constructor_ShouldCreateInstance()
+    {
+        // Act
+        var attribute = new DoNotMockAttribute();
+
+        // Assert
+        attribute.Should().NotBeNull();
+    }
+}
